ec259c4de97645b877a5c29e835c16b81162bf75
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-05 23:27:53 +0330

    feat: add dynamic book props to Book component
    - Introduced dynamic props for the Book component
    - Defined `firstBook` and `secondBook` objects to store book details
    - Passed title, author, and image as props instead of hardcoding them
    - Improved maintainability and scalability of the component

--- src/index.js
+++ src/index.js
@@ -3,35 +3,45 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const firstBook = {
+  author: "Dani kalafe",
+  title: "The Let Them Theory",
+  img: "https://m.media-amazon.com/images/I/91I1KDnK1kL._SL1500_.jpg",
+};
+
+const secoundBook = {
+  author: "Dr. Nicole Apelian",
+  title: "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips",
+  img: "https://m.media-amazon.com/images/I/91-E86oM2IL._SL1500_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        title={secoundBook.title}
+        author={secoundBook.author}
+        img={secoundBook.img}
+      />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props.img);
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg'
-    alt='The Let Them Theory'
-  />
-);
-const Title = () => <h2>The Let Them Theory</h2>;
-const Author = () => <h4>Mel Robins</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
