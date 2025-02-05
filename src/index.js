81c312caebaba69aba253f9e9c6fe3c43f5a2574
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-05 17:48:22 +0330

    all changed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
+const BookList = () => {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg'
+    alt='The Let Them Theory'
+  />
+);
+const Title = () => <h2>The Let Them Theory</h2>;
+const Author = () => <h4>Mel Robins</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
