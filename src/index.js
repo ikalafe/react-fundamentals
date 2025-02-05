de0f5de9d08c60a847a24541bf5f2c52c459b1b4
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-06 01:40:23 +0330

    refactor: improve BookList component with dynamic rendering
    - Replaced individual book objects with an array of books
    - Used `.map()` to dynamically render books instead of hardcoding each one
    - Enhanced Book component by destructuring props for cleaner code
    - Added `children` prop support to allow additional content inside Book component

--- src/index.js
+++ src/index.js
@@ -3,42 +3,39 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: "Dani kalafe",
-  title: "The Let Them Theory",
-  img: "https://m.media-amazon.com/images/I/91I1KDnK1kL._SL1500_.jpg",
-};
+const books = [
+  {
+    author: "Dr. Nicole Apelian",
+    title:
+      "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips",
+    img: "https://m.media-amazon.com/images/I/91-E86oM2IL._SL1500_.jpg",
+  },
 
-const secoundBook = {
-  author: "Dr. Nicole Apelian",
-  title: "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips",
-  img: "https://m.media-amazon.com/images/I/91-E86oM2IL._SL1500_.jpg",
-};
-
+  {
+    author: "Dani kalafe",
+    title: "The Let Them Theory",
+    img: "https://m.media-amazon.com/images/I/91I1KDnK1kL._SL1500_.jpg",
+  },
+];
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book
-        title={secoundBook.title}
-        author={secoundBook.author}
-        img={secoundBook.img}
-      />
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  console.log(props.img);
+  const { img, title, author, children } = props;
   return (
     <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
