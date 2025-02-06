291b7cf3eec5d8e06b59b00279eca979a366e01c
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-07 00:54:47 +0330

    refactor: improve BookList component with unique keys and prop spreading
    - Added a unique `id` property to each book object to optimize rendering
    - Used the `key` prop inside `.map()` to prevent React warnings
    - Implemented prop spreading (`{...book}`) for cleaner Book component usage

--- src/index.js
+++ src/index.js
@@ -9,33 +9,33 @@ const books = [
     title:
       "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips",
     img: "https://m.media-amazon.com/images/I/91-E86oM2IL._SL1500_.jpg",
+    id: 1,
   },
 
   {
     author: "Dani kalafe",
     title: "The Let Them Theory",
     img: "https://m.media-amazon.com/images/I/91I1KDnK1kL._SL1500_.jpg",
+    id: 2,
   },
 ];
 const BookList = () => {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
